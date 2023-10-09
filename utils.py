a837761b91eb13743a1ccdf5695e17ce3ffdefe3
Michael Eco <michael.gunawan2002@gmail.com>
2023-10-09 15:55:19 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 from nltk.stem import WordNetLemmatizer
 import string
 import re
-from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import pandas as pd
 import nltk
@@ -38,16 +38,16 @@ def preprocess(text):
     return text
 
 
-def count_frequency(documents, threshold=0.2):
-    vectorizer = TfidfVectorizer()
-    X = vectorizer.fit_transform(documents)
-    feature_names = vectorizer.get_feature_names_out()
-    tfidf_values = X.toarray()
-    filtered_words = np.array(feature_names)[
-        np.where(np.mean(tfidf_values, axis=0) < threshold)
-    ]
+# def count_frequency(documents, threshold=0.2):
+#     vectorizer = TfidfVectorizer()
+#     X = vectorizer.fit_transform(documents)
+#     feature_names = vectorizer.get_feature_names_out()
+#     tfidf_values = X.toarray()
+#     filtered_words = np.array(feature_names)[
+#         np.where(np.mean(tfidf_values, axis=0) < threshold)
+#     ]
 
-    return filtered_words
+#     return filtered_words
 
 
 def vectorize_diary(diary):
@@ -60,6 +60,6 @@ def vectorize_diary(diary):
 
     df = pd.DataFrame(documents, columns=["content"])
     df["content"] = df["content"].apply(preprocess)
-    filtered_words = count_frequency(list(df["content"].values))
-    df["content"] = df["content"].apply(filter)
+    # filtered_words = count_frequency(list(df["content"].values))
+    # df["content"] = df["content"].apply(filter)
     return df
